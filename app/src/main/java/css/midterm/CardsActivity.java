872c88d6524d7fde09903dfbc371b0c537c8a719
Peter Rubin <prubin@css.edu>
2021-03-14 23:48:47 -0500

    test

--- app/src/main/java/css/midterm/CardsActivity.java
+++ app/src/main/java/css/midterm/CardsActivity.java
@@ -10,5 +10,6 @@ public class CardsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
+        //test
     }
 }
